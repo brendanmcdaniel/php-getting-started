56cdb9d060de2b3462af428592ac0ff532debee0
Kemal DAG <gkemaldag@gmail.com>
2015-08-31 17:08:44 +0300

    pcntl added to heroku composer.json

--- test.php
+++ test.php
@@ -2,20 +2,19 @@
 $tmp = '';
 
 $handler = function($signal) use($tmp){
-    echo "\n signal captured!!! Commencing graceful shutdown\n";
-     switch($signal) {
-        case SIGTERM:
-          $cmd = 'pwd';
-          echo "SIGTERM IS CAPTURED running command $cmd \n";
-          exec($cmd, $output);
-          echo "Command executed : \n";
-          foreach ($output as $lineNo => $line) {
-            echo ($lineNo + 1) . ": $line \n";
-          }
-          exit(); //no need to do anything else since system is exited
-          break;
-    }
-
+  echo "\n signal captured!!! Commencing graceful shutdown\n";
+  switch($signal) {
+    case SIGTERM:
+      $cmd = 'pwd';
+      echo "SIGTERM IS CAPTURED running command $cmd \n";
+      exec($cmd, $output);
+      echo "Command executed : \n";
+      foreach ($output as $lineNo => $line) {
+        echo ($lineNo + 1) . ": $line \n";
+      }
+      exit(); //no need to do anything else since system is exited
+      break;
+  }
 };
 
 pcntl_signal(SIGTERM, $handler);
