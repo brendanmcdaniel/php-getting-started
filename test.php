4c0bfd292e363535c90530b896b576cc64da97f8
brendan <brendan.mcdaniel2013@gmail.com>
2015-09-07 12:23:07 +0100

    Amended output logs to show on paper trail

--- test.php
+++ test.php
@@ -1,15 +1,24 @@
 <?php
   
 $handler = function($signal) {
-  echo "Signal captured!!! Commencing graceful shutdown\n";
+    
+  //echo "Signal captured!!! Commencing graceful shutdown\n";
+  file_put_contents("php://stderr",  "Signal captured!!! Commencing graceful shutdown\n");
+  
   switch($signal) {
     case SIGTERM:
+        
       $cmd = 'pwd';
-      echo "SIGTERM IS CAPTURED running command $cmd \n";
+        
+      //echo "SIGTERM IS CAPTURED running command $cmd \n";
+      file_put_contents("php://stderr",  "SIGTERM IS CAPTURED running command $cmd \n");
+      
       exec($cmd, $output);
-      echo "Command executed : \n";
+      file_put_contents("php://stderr",  "Command executed : \n");
+      //echo "Command executed : \n";
       foreach ($output as $lineNo => $line) {
-        echo ($lineNo + 1) . ": $line \n";
+      file_put_contents("php://stderr",  ($lineNo + 1) . ": $line \n");
+      //echo ($lineNo + 1) . ": $line \n";
       }
       exit(); //no need to do anything else since system is going down!
       break;
@@ -17,7 +26,8 @@ $handler = function($signal) {
 };
 
 pcntl_signal(SIGTERM, $handler);
-echo "Now Listening for SIGTERM signal \n";
+//echo "Now Listening for SIGTERM signal \n";
+file_put_contents("php://stderr",  "Now Listening for SIGTERM signal \n");
 
 //now listen for signals for every 0.25 seconds
 while(true){
